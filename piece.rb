b8ac4b6d4e5958312392ad395477d8cf8ebfa147
Patrick Fulghum <patrick.c.fulghum@gmail.com>
2017-11-07 09:46:11 -0500

    Begin SmoothPiece Module

--- piece.rb
+++ piece.rb
@@ -5,3 +5,27 @@ require 'singleton'
 ROOK_QUEEN_KING = [[-1, 0], [0, -1], [1, 0], [0, 1]]
 BISHOP_QUEEN_KING = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
 KNIGHT_GOD = [[2, 1], [1, 2], [-1, 2], [-2, 1], [1, -2], [2, -1], [-1, -2], [-2, -1]]
+
+class Piece
+  attr_accessor :color, :position, :moved, :display_value
+
+  def initialize(board, position, color = nil, moved = false)
+    @board = board
+    @position = position
+    @color = color
+    @moved = moved
+  end
+
+  module SmoothMovement
+
+    def terminus(starting_pos, direction)
+      until @board
+    end
+
+    def legal_moves(directions)
+      return [] if directions == [0, 0]
+      legal_moves = []
+      directions.each do |direction|
+        @board
+      end
+    end
